0bd5aee5e663ae5514f6f9d3d48e6980ffc8c0fa
ezegyfa2 <darvasvok@gmail.com>
2021-10-25 23:49:06 +0200

    Fixing component importing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import BlackDescription from "./BlackDescription.vue";
-import Container from "./Container.vue";
-import WhiteDescription from "./WhiteDescription.vue";
-
 export default {
 	install(Vue) {
-		Vue.component('description-container', Container.default);
-		Vue.component('black-description', BlackDescription.default);
-		Vue.component('white-description', WhiteDescription.default);
+		Vue.component('description-container', require('./Container.vue').default);
+		Vue.component('black-description', require('./BlackDescription.vue').default);
+		Vue.component('white-description', require('./WhiteDescription.vue').default);
 	}
 };
