734dcb4d04023dfb3ed90167cd30fa5498b938eb
Darvas Istvan <darvasvok@gmail.com>
2023-01-02 21:36:52 +0200

    v1.0.4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export default {
 	install(Vue) {
-		Vue.component('description-section-container', require('./Container/Component.vue').default);
-		Vue.component('black-description-section', require('./BlackDescription/Component.vue').default);
-		Vue.component('white-description-section', require('./WhiteDescription/Component.vue').default);
+		registerVueComponent('description-section-container', require('./Container/Component.vue').default, Vue);
+		registerVueComponent('black-description-section', require('./BlackDescription/Component.vue').default, Vue);
+		registerVueComponent('white-description-section', require('./WhiteDescription/Component.vue').default, Vue);
 	}
 };
