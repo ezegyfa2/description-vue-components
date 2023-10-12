24af0df126832afb0175166b0ccd7a157ae2d517
ezegyfa2 <darvasvok@gmail.com>
2023-10-12 17:40:13 +0300

    Lazy image

--- src/WhiteDescription/index.js
+++ src/WhiteDescription/index.js
@@ -2,6 +2,7 @@ export default {
     install(Vue) {
         require('../Description').default.install(Vue)
 
+        Vue.component('b-lazy-image', () => import('bootstrap-vue').then(module => module.BImgLazy))
         Vue.component('white-description-section', () => import('./Component.vue'))
     }
 }
