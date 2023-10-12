3e3a40f7d563af36f41cc834efd49d2ee744c520
József Bagota <jozsef.bagota@gmail.com>
2023-10-12 17:24:55 +0200

    adding vue-image component

--- src/BlackDescription/index.js
+++ src/BlackDescription/index.js
@@ -1,6 +1,7 @@
 export default {
     install(Vue) {
         require('../Description').default.install(Vue)
+        require('helper-vue-components/image').default.install(Vue)
 
         Vue.component('black-description-section', () => import('./Component.vue'))
     }
--- src/WhiteDescription/index.js
+++ src/WhiteDescription/index.js
@@ -1,6 +1,7 @@
 export default {
     install(Vue) {
         require('../Description').default.install(Vue)
+        require('helper-vue-components/image').default.install(Vue)
 
         Vue.component('b-lazy-image', () => import('bootstrap-vue').then(module => module.BImgLazy))
         Vue.component('white-description-section', () => import('./Component.vue'))
